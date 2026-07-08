6300b6a61a5a49bf0b2b110c2a951d600dcb2ac4
Luciano Rossi <lukio@gcoop.coop>
2026-07-08 04:54:30 -0300

    Use to register in the Pool

--- __init__.py
+++ __init__.py
@@ -1,32 +1,3 @@
 # This file is part of the account_move_summary module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import account
-from . import move
-
-__all__ = ['register']
-
-
-def register():
-    Pool.register(
-        account.Period,
-        account.FiscalYear,
-        move.Summary,
-        move.SummaryPeriod,
-        move.SummaryMove,
-        move.SummaryLine,
-        move.Move,
-        move.RenumberSummaryMovesStart,
-        move.PrintSummaryGeneralJournalStart,
-        module='account_move_summary', type_='model')
-    Pool.register(
-        account.RenewFiscalYear,
-        move.RenumberSummaryMoves,
-        move.PrintSummaryGeneralJournal,
-        module='account_move_summary', type_='wizard')
-    Pool.register(
-        move.SummaryGeneralJournalPDF,
-        move.SummaryGeneralJournalXLS,
-        module='account_move_summary', type_='report')
